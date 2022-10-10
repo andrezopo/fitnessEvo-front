17a99e23325a3a02cf4ff2969958c47a34c30b17
André Zopolato <andrezopo@gmail.com>
2022-10-09 23:39:21 -0300

    :sparkles: feat: create Header with sing-out button

---
+++ src/components/Header.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import fitnessEvo from "../assets/images/fitnessEvo.png";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../contexts/UserContext";
+
+function Header({ text, signOutButton }) {
+  const { name } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  function signOut() {
+    navigate("/", { replace: true });
+  }
+  return (
+    <StyledDiv signOutButton={signOutButton}>
+      <div>{text}</div>
+      <div>
+        <img onClick={signOut} src={fitnessEvo} alt="Figura de logout" />
+        <p>Sair</p>
+      </div>
+    </StyledDiv>
+  );
+}
+
+const StyledDiv = styled.div`
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  height: 80px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #602323;
+  font-weight: 700;
+  font-size: 24px;
+  padding: 0 25px;
+  color: #ffffff;
+
+  img {
+    visibility: ${(props) => (props.signOutButton ? "initial" : "hidden")};
+    width: 40px;
+    height: 40px;
+    margin: 0;
+  }
+
+  p {
+    font-size: 18px;
+    font-weight: 400;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export default Header;
