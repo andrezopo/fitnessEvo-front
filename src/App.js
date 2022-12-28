a2ab5079b3bc673f0f1124f5f4cab108852fc22a
André Zopolato <andrezopo@gmail.com>
2022-12-27 23:18:06 -0300

    :sparkles: feat: add new page route /today

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ import UserAddtionalInfos from "./pages/UserAdditionalInfos";
 import UserContext from "./contexts/UserContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import UserTodayReport from "./pages/UserTodayReport";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#602323",
+      light: "#913b3b",
+      dark: "#381717",
+    },
+    secondary: {
+      main: "#002aaa",
+      light: "#5578e0",
+      dark: "042589",
+    },
+  },
+});
 
 export default function App() {
   const [email, setEmail] = useState("");
@@ -15,27 +32,30 @@ export default function App() {
   const [userId, setUserId] = useState("");
   return (
     <BrowserRouter>
-      <ToastContainer theme="colored" autoClose={2000} />
-      <UserContext.Provider
-        value={{
-          email,
-          setEmail,
-          password,
-          setPassword,
-          name,
-          setName,
-          token,
-          setToken,
-          userId,
-          setUserId,
-        }}
-      >
-        <Routes>
-          <Route path="/" element={<SignInScreen />} />
-          <Route path="/sign-up" element={<SignUpScreen />} />
-          <Route path="/user" element={<UserAddtionalInfos />} />
-        </Routes>
-      </UserContext.Provider>
+      <ThemeProvider theme={theme}>
+        <ToastContainer theme="colored" autoClose={2000} />
+        <UserContext.Provider
+          value={{
+            email,
+            setEmail,
+            password,
+            setPassword,
+            name,
+            setName,
+            token,
+            setToken,
+            userId,
+            setUserId,
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<SignInScreen />} />
+            <Route path="/sign-up" element={<SignUpScreen />} />
+            <Route path="/user" element={<UserAddtionalInfos />} />
+            <Route path="/today" element={<UserTodayReport />} />
+          </Routes>
+        </UserContext.Provider>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
